58545987be08e0ba94b3b3d6aa7f0762418b3cb6
amxra <amiradediran@gmail.com>
2019-10-15 20:43:41 +0100

    Add private route

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import LogIn from './components/login'
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Redirect, withRouter } from 'react-router-dom';
 import Friends from './components/friends';
 import NewFriend from './components/newfriend';
 
 
 import './App.css';
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <nav className="nav">
@@ -18,12 +18,20 @@ function App() {
 
       <main>
         <Route exact path='/' component={LogIn} />
-        <Route exact path='/friends' component={Friends} />
-        <Route path='/newFriend' component={NewFriend} />
+        <Route path='/friends' render={props => withAuthCheck(Friends, props) } />
+        <Route path='/newFriend' render={props => withAuthCheck(NewFriend, props) } />
 
       </main>
     </div>
   );
 }
+function withAuthCheck (Component, props) {
+  if (localStorage.getItem('token')) {
+    return <Component {...props} />
+  }
+  else {
+    return <Redirect to='/' />;
+  }
+}
 
-export default App;
+export default withRouter(App);
