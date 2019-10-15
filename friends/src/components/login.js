c7143183c5cfe4731724740adc28288c4dd3a3e9
amxra <amiradediran@gmail.com>
2019-10-15 14:38:11 +0100

    install axios and react router dom

--- friends/src/components/login.js
+++ friends/src/components/login.js
@@ -1,6 +1,22 @@
-import react from 'react'
+import React, {useRef} from 'react'
 
 export default class LogIn extends React.Component {
+
+    constructor(){
+
+        //useRef hook returns mutable ref whose current property is initialised to the passed argument.
+
+        const usernameRef = useRef();
+        const passwordRed = useRef();
+
+        const onSubmit = () => {
+            axios.post('', {
+                username: usernameRef.current.value,
+                password: passwordRef.current.value,
+            }), 
+        }
+    }
+
     render(){
         return (
             <form>
