3a2b8e8fda6ee0df99666e3ad61717d1408e6c3b
amxra <amiradediran@gmail.com>
2019-10-15 20:30:18 +0100

    render friend from api

--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -1,22 +1,26 @@
 import React, {useState, useEffect}from 'react';
 import axios from 'axios'
+import AxiosAuth from '../axiosauth';
+import Friend from './friend'
 
 
 export default function  () {
     const [ friends, setFriends ] = useState([])
 
     useEffect(() => {
-        axios.get('http://localhost:5000/api/friends')
+        AxiosAuth().get('http://localhost:5000/api/friends')
             .then(res => {
                 console.log(res);
             })
             .catch(err => {
                 console.log(err)
             })
-    })
+    }, [])
     return(
         <div className = 'friends'>
-
+             {
+                friends.map(friend => <Friend key={friend.id} friend={friend} /> )
+            }
         </div>
     )
 }
